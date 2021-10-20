8228d7de511ba6a47b3dada0f3618dde4cc9f190
pountzas <nikos@pountzas.gr>
2021-10-20 20:04:06 +0300

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  namespace :v1, defaults: { format: :json } do
-    get 'things', to: 'things#index'
+  namespace :v1, defaults: { format: 'json' } do
+    get 'greetings', to: 'greetings#index'
   end
-  get '*page', to: 'static#index', constraints: lambda { |req|
+  get '*page', to: 'static#index', constraints: ->(req) do
     !req.xhr? && req.format.html?
-  }
-
+  end
   root 'static#index'
 end
